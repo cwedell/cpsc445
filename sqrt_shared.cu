db995aae6bfc108f2a9561714002fd25ca2b8053
Colton Wedell <44662526+cwedell@users.noreply.github.com>
2021-12-05 22:49:53 -0800

    Updated sqrt_shared.cu

--- sqrt_shared.cu
+++ sqrt_shared.cu
@@ -8,7 +8,7 @@
 using namespace std;
 
 __global__ void sqrtcalc(float* inputs, int size) {
-  printf("my rank is %d\n", myrank);
+  printf("my rank is %f\n", myrank);
 	int myrank = blockIdx.x * blockDim.x + threadIdx.x;
 	extern __shared__ float shinputs[];
 	if(myrank < size) {
